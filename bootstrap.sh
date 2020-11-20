62cf0ef18a0b55324d47859241b9f89e35b0f088
Craig Lockwood <clockwood@terakeet.com>
2020-11-20 16:04:34 -0500

    rework/optimize/streamline config files. clean things up.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,26 +5,26 @@ cd "$(dirname "${BASH_SOURCE}")";
 # Make sure repo is up to date
 git pull origin master;
 
-# Copy fonts to system folder
-rsync --exclude ".DS_Store" -avh --no-perms resources/fonts/. ~/Library/Fonts/
-
 function copy_files() {
-	rsync --exclude ".git/" \
-    --exclude "helpers/" \
-    --exclude "resources/" \
-		--exclude ".DS_Store" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		-avh --update --no-perms . ~;
+  # Copy dev fonts
+  rsync -avh --no-perms --exclude ".DS_Store" resources/fonts/. ~/Library/Fonts/;
+  # Copy dotfiles
+  rsync --exclude ".git/" \
+  --exclude "helpers/" \
+  --exclude ".DS_Store" \
+  --exclude "bootstrap.sh" \
+  --exclude "README.md" \
+  -avh --update --no-perms . ~;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	copy_files;
+  copy_files;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		copy_files;
-	fi;
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    copy_files;
+  fi;
 fi;
+
 unset copy_files;
